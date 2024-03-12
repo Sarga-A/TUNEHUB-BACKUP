fa61fcc9834af721cdd343410273fa3715491855
Sarga-A <106979481+Sarga-A@users.noreply.github.com>
2024-03-12 11:42:40 +0530

    Add files via upload

---
+++ FullStackProjectTunehub/src/main/java/com/kodnest/tunehub/service/SongService.java
@@ -0,0 +1,19 @@
+package com.kodnest.tunehub.service;
+
+import java.util.List;
+
+import com.kodnest.tunehub.entity.Song;
+
+public interface SongService {
+
+	public void addSong(Song song);
+
+	public boolean songExists(String name);
+
+	public List<Song> fetchAllSongs();
+
+	public void updateSong(Song s);
+
+	
+
+}
